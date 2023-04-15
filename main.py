d57b9f958b9cf1034c547028e9e4d57edb1098f7
Dream <wang-ry22@mails.tsinghua.edu.cn>
2023-04-15 14:43:02 +0800

    push commit

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('Test Git3-hot-fix-master')
     print_hi('Test Git3-hot-fix2')
+    print_hi('Test push')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
