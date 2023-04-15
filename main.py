ade3a3b9f6b7310bd8e8d08bc56b854d1fbcdbcb
Dream <wang-ry22@mails.tsinghua.edu.cn>
2023-04-15 14:15:48 +0800

    hot-fix second commit

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Test Git3-hot-fix')
+    print_hi('Test Git3-hot-fix2')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
